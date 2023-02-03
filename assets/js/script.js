0bf7b07635e3626d049e074749b09285751eb0e1
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-02-03 19:06:15 +0000

    Add DOM selectors for elements and event listener for search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+let formContainer = document.querySelector("#search-form");
+let locationInputEl = document.querySelector("#location");
+let fromDateInputEl = document.querySelector("#from-date");
+let toDateInputEl = document.querySelector("#to-date");
+let searchBtnEl = document.querySelector("#search-btn");
+
+searchBtnEl.addEventListener("click", function (e) {
+  e.preventDefault();
+});
+
 $(function () {
   $(".datepicker").datepicker();
 });
