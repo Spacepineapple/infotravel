fc29b0d3a38f6b48744501a9fd35327fb0a5bdf2
Spacepineapple <neal.t.reeves@gmail.com>
2023-02-03 20:00:31 +0000

    Create function to news data to newsfeed section

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,10 @@ let locationInputEl = document.querySelector("#location");
 let fromDateInputEl = document.querySelector("#from-date");
 let toDateInputEl = document.querySelector("#to-date");
 let searchBtnEl = document.querySelector("#search-btn");
+let newsCards = document.querySelectorAll(".news-card");
+let newsAPIKey = "pub_165518ddbc391a0563b33c28f98a88bc39c78"
+let cityName = "London"
+let newsURL = `https://newsdata.io/api/1/news?apikey=${newsAPIKey}&language=en&qInTitle=${cityName}`
 
 searchBtnEl.addEventListener("click", function (e) {
   e.preventDefault();
@@ -11,3 +15,25 @@ searchBtnEl.addEventListener("click", function (e) {
 $(function () {
   $(".datepicker").datepicker();
 });
+
+function getNewsHeadlines() {
+  fetch(newsURL)
+  .then((response) => response.json())
+  .then((newsData) => {
+    console.log(newsData);
+    for (let i=0; i<3; i++) {
+      const card = newsCards[i];
+      console.log(card);
+      const title = card.querySelector(".card-body").querySelector(".card-title");
+      title.textContent = newsData.results[i].title;
+      const newsText = card.querySelector(".card-body").querySelector(".card-text");
+      newsText.textContent = newsData.results[i].description;
+      const newsImage = card.querySelector(".card-img-top");
+      newsImage.setAttribute("src", newsData.results[i].image_url);
+      console.log(newsData.results[i].title);
+      console.log(newsData.results[i].description);
+    }
+  });
+}
+
+getNewsHeadlines()
