edbeb7212123732c01ab585d93c1e51a843f8f38
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-02-04 18:50:47 +0000

    Add function to fetch city data
    The getWeather function returns the generic city data, which in turn calls on the getWeatherData function to fetch the weather information object.
    Also added a function to get the current user location which can be used to populate the page with data before a search is carried out. This was achieved using the built-in geolocation api.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,50 @@ let fromDateInputEl = document.querySelector("#from-date");
 let toDateInputEl = document.querySelector("#to-date");
 let searchBtnEl = document.querySelector("#search-btn");
 
+let apiKey = "40640050a45cbd8cf8d35ada1e14fee3";
+
+/** Get user's location */
+let userLocation = {};
+if (window.navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+      userLocation.latitude = latitude;
+      userLocation.longitude = longitude;
+    },
+    (err) => console.log(err)
+  );
+}
+
+/** Fetch Weather data */
+function getWeather(searchCity) {
+  let queryUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${searchCity}&limit=5&appid=${apiKey}`;
+
+  fetch(queryUrl)
+    .then((res) => res.json())
+    .then((data) => {
+      let cityData = data[0];
+      let cityLon = cityData.lon;
+      let cityLat = cityData.lat;
+      getWeatherData(cityLat, cityLon);
+    });
+}
+
+function getWeatherData(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
+  )
+    .then((res) => res.json())
+    .then((weatherData) => {
+      let weatherInfo = weatherData.list;
+      console.log(weatherInfo);
+    });
+}
+
 searchBtnEl.addEventListener("click", function (e) {
   e.preventDefault();
+  console.log("Search button clicked");
+  getWeather(locationInputEl.value);
 });
 
 $(function () {
