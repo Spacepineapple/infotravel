0e963c4e95f83b9a836df29c874b05fcd08528bd
Osman Dumbuya <dumbuyaosman@icloud.com>
2023-02-05 21:09:58 +0000

    Add data values for the five days forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ let locationInputEl = document.querySelector("#location");
 let fromDateInputEl = document.querySelector("#from-date");
 let toDateInputEl = document.querySelector("#to-date");
 let searchBtnEl = document.querySelector("#search-btn");
-let currentLocationEl = document.querySelector("#location");
+let currentLocationEl = document.querySelector("#current-location");
 let currentDateEl = document.querySelector("#current-date");
 let weatherDescriptionEl = document.querySelector("#weather-description");
 let currentWeatherIconEl = document.querySelector("#current-weather-icon");
@@ -14,6 +14,7 @@ let feelsLikeValueEl = document.querySelector("#fl-value");
 let windValueEl = document.querySelector("#wind-value");
 let humidityValueEl = document.querySelector("#humidity-value");
 let visibilityValueEl = document.querySelector("#visibility-value");
+let forecastContainerEl = document.querySelector("#forecast-container");
 
 let apiKey = "40640050a45cbd8cf8d35ada1e14fee3";
 
@@ -38,6 +39,9 @@ function getWeather(searchCity) {
     .then((res) => res.json())
     .then((data) => {
       let cityData = data[0];
+
+      currentLocationEl.textContent = cityData.name;
+
       let cityLon = cityData.lon;
       let cityLat = cityData.lat;
       getWeatherData(cityLat, cityLon);
@@ -69,12 +73,33 @@ function getWeatherData(lat, lon) {
       windValueEl.textContent = weatherInfo[0].wind.speed;
 
       // Display the future weather forecast data on the screen
+      let fiveDayForecastArr = [
+        weatherInfo[8],
+        weatherInfo[16],
+        weatherInfo[24],
+        weatherInfo[32],
+        weatherInfo[39],
+      ];
+      fiveDayForecastArr.forEach((forecast) => {
+        let day = moment(forecast.dt, "X").format("dddd");
+        let forecastWeatherIconSrc = forecast.weather[0].icon;
+        let forecastTemp = forecast.main.temp.toFixed();
+        forecastContainerEl.innerHTML += `
+          <div class="forecast-card card">
+            <img class="weather-icon" src="http://openweathermap.org/img/wn/${forecastWeatherIconSrc}@2x.png" alt="Weather Icon" id="current-weather-icon">
+            <div class="forecast-day">${day}</div>
+            <div>${forecastTemp}&deg;</div>
+          </div>
+        `;
+      });
     });
 }
 
 searchBtnEl.addEventListener("click", function (e) {
   e.preventDefault();
   console.log("Search button clicked");
+  currentLocationEl.textContent = "";
+  forecastContainerEl.innerHTML = "";
   getWeather(locationInputEl.value);
 });
 
